34c5448a1cd572a39fe38593a94910c9a4f49b5a
Olomo korede Michael <michealolomo@gmail.com>
2024-11-11 13:31:05 +0100

    Website with React

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <footer style={{ padding: '10px', backgroundColor: '#333', color: '#fff', textAlign: 'center' }}>
+            <p>© 2023 My Company. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
+
